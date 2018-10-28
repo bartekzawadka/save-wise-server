46ab300ff6d29242ea2c8e61da59e89be7a37640
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2018-10-28 02:27:14 +0200

    Changed name convention

--- SaveWise/SaveWise.Api/Startup.cs
+++ SaveWise/SaveWise.Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace save_wise_server
+namespace SaveWise.Api
 {
     public class Startup
     {
